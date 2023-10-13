63e54ca3b570980907248c608dede27be5e8b5be
tentaShiratori <tenta.shiratori@gmail.com>
2023-10-14 02:57:46 +0900

    write message to test channel

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,16 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import pkg from '@slack/bolt';
+const { App } = pkg;
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const app = new App({
+    token:process.env.SLACK_BOT_TOKEN,
+    signingSecret:process.env.SLACK_SIGNING_SECRET
+  })
+
+  await app.client.chat.postMessage({
+    channel:"C060MDTT9QX",
+    text:"test message"
+  })
   return res.json("Hello World")
 }
